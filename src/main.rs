6a78ddc87e86d5a6cf6fdc9aa6c07e73f3582b67
Dylan Hardison <dylan@hardison.net>
2024-01-01 21:14:56 -0800

    clean up the "database" code a bit, and only use a single Arc.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
 mod args;
+mod db;
 mod parser;
 
 use args::Args;
-use axum::{extract::State, routing::get, Json, Router};
-use chrono::{DateTime, Utc};
-use dashmap::{DashMap, DashSet};
+use axum::{
+    extract::{Path, State},
+    routing::get,
+    Json, Router,
+};
+use db::Database;
 use linemux::MuxedLines;
-use parser::{Action, Event};
 use serde_json::{json, Value};
-use std::{sync::Arc, io::IsTerminal};
+use std::{io::IsTerminal, sync::Arc};
 use tokio::{net::TcpListener, signal};
 use tower_http::trace::TraceLayer;
-
-type LocationMap = DashMap<String, DashSet<String>>;
-type LastSeen = DashMap<String, DateTime<Utc>>;
-
-#[derive(Debug, Clone)]
-struct Database {
-    location: Arc<LocationMap>,
-    last_seen: Arc<LastSeen>,
-}
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -33,12 +27,7 @@ pub enum Error {
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args = Args::new();
-    let location = Arc::new(DashMap::new());
-    let last_seen = Arc::new(DashMap::new());
-    let db = Database {
-        location: location.clone(),
-        last_seen: last_seen.clone(),
-    };
+    let db = Arc::new(Database::new());
 
     let subscriber = tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
@@ -66,13 +55,14 @@ async fn main() -> Result<(), Error> {
                     continue;
                 };
                 tracing::trace!("line: {}", line.line());
-                process_event(&db, event);
+                db.witness(event);
             }
         });
     }
 
     let router = Router::new()
         .route("/", get(index))
+        .route("/mac/:mac", get(lookup_mac))
         .with_state(db)
         .layer(TraceLayer::new_for_http());
     let listener = TcpListener::bind(&args.listen).await?;
@@ -83,32 +73,17 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn process_event(db: &Database, event: Event) {
-    let timestamp = event.timestamp;
-    let host = event.host;
-    match event.action {
-        Action::Associated { mac } | Action::Observed { mac } => {
-            tracing::info!("{timestamp} add {host} {mac}");
-            db.last_seen.insert(mac.clone(), timestamp);
-            db.location.entry(mac).or_default().insert(host);
-        }
-        Action::Disassociated { mac } => {
-            tracing::info!("{timestamp} remove {host} {mac}");
-            if let Some(hosts) = db.location.get_mut(&mac) {
-                hosts.remove(&host);
-            }
-        }
-        Action::Junk(msg) => {
-            tracing::error!("{timestamp} junk {host} {msg}");
-        }
-        Action::Ignored => {}
-    }
+async fn index(State(db): State<Arc<Database>>) -> Json<Value> {
+    Json(json!({
+        "location": &db.location,
+        "last_seen": &db.last_seen,
+    }))
 }
 
-async fn index(State(db): State<Database>) -> Json<Value> {
+async fn lookup_mac(State(db): State<Arc<Database>>, Path(mac): Path<String>) -> Json<Value> {
     Json(json!({
-        "location": *(db.location),
-        "last_seen": *(db.last_seen),
+        "location": db.location.get(&mac),
+        "last_seen": db.last_seen.get(&mac),
     }))
 }
 
